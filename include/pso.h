093175d4ea655d0f2c7f75aba3b699499714b932
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2020-09-15 19:53:33 -0300

    Swarm data structure.

--- include/pso.h
+++ include/pso.h
@@ -27,3 +27,14 @@ struct particle {
 particle_t *particle_create(limits_t *dimensions_limits);
 
 void particle_destroy(particle_t *particle);
+
+typedef struct swarm {
+    particle_t *particles;
+    int nr_particles;
+    limits_t search_space;
+
+    // each position `i` in this array corresponds to a posisition in the
+    // `particles` array and will contain an array of pointers to the particles
+    // which will inform `particles[i]`
+    particle_t *topology[];
+} swarm_t;
