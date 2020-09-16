eb408253f3efdaac59256cae6b4f8b1f09cf2e0f
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2020-09-16 17:11:31 -0300

    `neighbourhood` data structure.

--- include/pso.h
+++ include/pso.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <stdbool.h>
+typedef struct particle particle_t;
 
 typedef struct dimension {
     double min;
@@ -12,29 +12,31 @@ typedef struct limits {
     int nr_dimensions;
 } limits_t;
 
-typedef struct particle particle_t;
+typedef struct neighbourhood {
+    particle_t **neighbours;    // an array of pointers to particles
+    int nr_neighbours;
+} neighbourhood_t;
+
 struct particle {
     double *x;                  // an array of nr_dimensions elements
-
     double fitness;
     double *velocity;           // an array of nr_dimensions elements
     particle_t *previous_best;
+
+    neighbourhood_t neighbours;
 };
-
-/**
- * Creates an empty new particle.
- */
-particle_t *particle_create(limits_t *dimensions_limits);
-
-void particle_destroy(particle_t *particle);
 
 typedef struct swarm {
     particle_t *particles;
     int nr_particles;
     limits_t search_space;
-
-    // each position `i` in this array corresponds to a posisition in the
-    // `particles` array and will contain an array of pointers to the particles
-    // which will inform `particles[i]`
-    particle_t *topology[];
 } swarm_t;
+
+/**
+ * Creates an empty new particle.
+ *
+ * \return The pointer to the particle created or NULL if creation fails.
+ */
+particle_t *particle_create(limits_t *dimensions_limits);
+
+void particle_destroy(particle_t *particle);
