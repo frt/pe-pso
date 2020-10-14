40f5e39b26957a80d84ceb229ce986faba7ccd65
Fábio Roberto Teodoro <fr.teodoro@gmail.com>
2020-10-13 21:23:55 -0300

    Velocity and position update.

--- src/pso.c
+++ src/pso.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <math.h>
 
 #include <randistrs.h>
 
@@ -165,13 +166,79 @@ void swarm_destroy(swarm_t *swarm)
     free(swarm);
 }
 
+// updates velocity and move particle
+bool move_particle(particle_t *particle, int nr_dimensions, double c, double w)
+{
+    int i;
+    double *gravity_center, *x1;
+    double *x = particle->x;
+    double *p = particle->previous_best_x;
+    double *l = particle->neighbourhood_best_x;
+    double radius, r, length;
+
+    gravity_center = (double *)malloc(nr_dimensions * sizeof(double));
+    x1             = (double *)malloc(nr_dimensions * sizeof(double));
+    if (gravity_center == NULL || x1 == NULL) {
+        free(gravity_center);
+        free(x1);
+        return false;
+    }
+
+    // calculate the gravity center
+    for (i = 0; i < nr_dimensions; ++i) {
+        gravity_center[i] = x[i] + c * ((p[i] + l[i] - 2 * x[i]) / 3);
+    }
+
+    // radius ||Gi − xi|| around gravity_center makes a hypersphere
+    for (i = 0, radius = 0; i < nr_dimensions; ++i)
+        radius += pow((gravity_center[i] - x[i]), 2);
+    radius = sqrt(radius);
+
+    /* get a random point x1 inside the hypersphere
+       [https://baezortega.github.io/2018/10/14/hypersphere-sampling/]
+    */
+    // 1. define the direction
+    for (i = 0, length = 0; i < nr_dimensions; ++i) {
+        x1[i] = rd_normal(0, 1);    // μ = 0 and σ = 1
+        length += pow(x1[i], 2);
+    }
+    length = sqrt(length);
+
+    // 2. define radius normalized in [0, 1)
+    r = pow(rd_uniform(0, 1), 1 / nr_dimensions);
+
+    // 3. apply the random radius and move the point toward the gravity center
+    for (i = 0; i < nr_dimensions; ++i)
+        x1[i] = gravity_center[i] + radius * r * (x1[i] / length);
+
+    // vi(t+1) = w * vi(t) + x'i(t) − xi(t)
+    // xi(t+1) = xi(t) + vi(t+1)
+    for (i = 0; i < nr_dimensions; ++i) {
+        particle->velocity[i] = w * particle->velocity[i] + x1[i] - x[i];
+        particle->x[i] += particle->velocity[i];
+    }
+
+    free(gravity_center);
+    free(x1);
+    return true;
+}
+
 int iterarions(swarm_t *swarm, pso_config_t *pso_config, int nr_iterations)
 {
     int i;
     double new_best_fitness;
 
+    /* Maurice Clerc.
+       Stagnation analysis in particle swarm optimization or
+       what happens when nothing happens, http://hal.archives-ouvertes.fr/hal-00122031.
+       Technical report, 2006. */
+    static const double w = 1 / (2 * log(2));
+    static const double c = 1 / 2 + log(2);
+
     // update particles positions
     for (i = 0; i < nr_iterations; ++i) {
+        move_particle(swarm->particles[i], swarm->search_space->nr_dimensions, c, w);
+        // XXX calculate new fitness, etc.
     }
 
     // update particles neighbourhoods
